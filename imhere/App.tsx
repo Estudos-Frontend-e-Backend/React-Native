871f32cda1d7b5f32e57791da38834899989930e
Mathlencar <matheus.alencar@ofsolution.com.br>
2023-03-23 15:09:45 -0300

    finish project app list

--- imhere/App.tsx
+++ imhere/App.tsx
@@ -1,33 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+
+import { StatusBar } from "react-native";
+import { Home } from "./src/screens/Home";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>
-        React
-      </Text>
-
-      <Text style={styles.eventDate}>
-        Sexta, 4 de Novembro de 2022
-      </Text>
-    </View>
+    <>
+      <StatusBar 
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+      />
+      <Home />
+    </>
   );
 }
-
-const styles = StyleSheet.create ({
-  container: {
-    flex: 1,
-    backgroundColor: '#131016',
-    padding: 24,
-  },
-  eventName: {
-    color: '#FDFCFE',
-    fontSize: 24, 
-    fontWeight: 'bold',
-    marginTop: 48,
-  },
-  eventDate: {
-    color: '#6b6b6b', 
-    fontSize: 16,
-  },
-})
